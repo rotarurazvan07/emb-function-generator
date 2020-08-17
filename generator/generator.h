af7285715a694576e69b6ac16df71e53750c111c
Razvan Rotaru <razvan-andrei.rotaru@nxp.com>
2020-08-17 19:48:47 +0300

    Add function choosing via a select cycling button and a start/stop button via interrupts

--- generator/generator.h
+++ generator/generator.h
@@ -1,1 +1,6 @@
-#define GENERATOR_DUTY_CYCLE
+#define GENERATOR_WAVES_NUMBER 4
+
+#define wave_select_button 2
+#define wave_utility_button 3
+
+char *waves_types[] = {"square", "triangle", "sin", "sawtooth"};
--- generator/generator.ino
+++ generator/generator.ino
@@ -1,11 +1,73 @@
 #include "generator.h"
 
+volatile int waves_types_index = 0;
+String wave_type_current = "";
+volatile bool wave_signaling = false;
+
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(9600);
+    Serial.println("Starting debug!");
+
+    pinMode(wave_select_button, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(wave_select_button), wave_select_btn_ISR, FALLING);
+
+    pinMode(wave_utility_button, INPUT_PULLUP);
+    attachInterrupt(digitalPinToInterrupt(wave_utility_button), wave_utility_btn_ISR, FALLING);
 }
 
 void loop() {
-    // put your main code here, to run repeatedly:
-    Serial.println(GENERATOR_DUTY_CYCLE);
+    if (wave_signaling)
+        Serial.println(String("Signaling: ") + wave_type_current);
+    delay(500);
+}
+
+void wave_utility_btn_ISR()
+{
+    static unsigned long last_interrupt_time = 0;
+    unsigned long interrupt_time = millis();
+
+    // If interrupts come faster than 200ms, assume it's a bounce and ignore
+    if (interrupt_time - last_interrupt_time > 300)
+    {
+        if (wave_signaling == false)
+        {
+            if (wave_type_current == "")
+                Serial.println("Please select a wave type, there is no default");
+            else
+            {
+                detachInterrupt(digitalPinToInterrupt(wave_select_button));
+                Serial.print("Selected: ");
+                Serial.println(wave_type_current);
+                wave_signaling = true;
+            }
+        }
+        else
+        {
+            wave_signaling = false;
+            Serial.println("Exiting current signal run");
+
+            // Clear interrupt flag
+            EIFR = bit(INTF0);
+
+            attachInterrupt(digitalPinToInterrupt(wave_select_button), wave_select_btn_ISR, FALLING);
+        }
+    }
+    last_interrupt_time = interrupt_time;
+}
+
+void wave_select_btn_ISR()
+{
+    static unsigned long last_interrupt_time = 0;
+    unsigned long interrupt_time = millis();
+
+    // If interrupts come faster than 200ms, assume it's a bounce and ignore
+    if (interrupt_time - last_interrupt_time > 300)
+    {
+        if (waves_types_index == GENERATOR_WAVES_NUMBER)
+            waves_types_index = 0;
+        wave_type_current = String(waves_types[waves_types_index++]);
+        Serial.println(wave_type_current);
+    }
+    last_interrupt_time = interrupt_time;
 }
